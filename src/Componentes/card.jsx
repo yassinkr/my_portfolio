2527250e46398f6161dc9db8a4b484c2815667df
Yassin Kr <kerraiyassin@gmail.com>
2023-11-16 21:45:52 +0100

    Initial commit

--- src/Componentes/card.jsx
+++ src/Componentes/card.jsx
@@ -22,11 +22,11 @@ const Card = ({ color,title,desc }) => {
 
   return (
     <div className={`w-[250px] h-[342px] rounded-b-[125px] bg-${color}-400 text-grey-400 relative `}>
-      <div className={`bg-gradient-to-b from-${color}-400 to-${color}-200 w-full h-[142px] border-[9px] border-${color}-500 text-4xl text-grey-400 flex justify-center items-center `}>
+      <div className={`bg-gradient-to-b from-${color}-500 to-${color}-300 w-full h-[142px] border-[9px] border-${color}-500 text-4xl text-grey-400 flex justify-center items-center `}>
        {title}
       </div>
       {svg && (
-        <div className='rounded-full w-[97px] h-[97px] bg-grey-400 flex justify-center items-center absolute top-[93px] left-[72px]'>
+        <div className='rounded-full w-[97px] h-[97px] bg-grey-400 flex justify-center items-center absolute top-[93px] left-[72px] shadow-lg shadow-black-400'>
           {svg}
         </div>
       )}
--- src/Componentes/cards.jsx
+++ src/Componentes/cards.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import Card from './card'
 const cards = () => {
   return (
-    <div className='w-full  h-screen relative flex justify-center align-middle items-center'>
+    <div className='w-full  h-screen relative grid'>
       <div className='w-full  h-screen blur-[125px] absolute overflow-hidden bg-opacity-100'>
        <div className='relativ bg-opacity-100'>
         <div className='rounded-full w-[562px] h-[562px] bg-blue-400 left-[500px] top-[672px] absolute'></div>
@@ -14,11 +14,26 @@ const cards = () => {
 
         <div className='rounded-full w-[562px] h-[562px] bg-blue-400 left-[-291px] top-[-316px] absolute'></div></div>
       </div>
+      <div className='flex justify-center align-middle items-center'>
+              <div className=''>
+      <div className='py-[70px]'> 
+        <div className='text-blue-400 text-3xl text-center'>
+        Skills & professions 
+        </div>
+        <div className='text-black-400 text-2xl text-center'>
+        exploit and highlight my skills on creating amazing digital experiences  
+        </div>
+      </div>
+      
+
       <div className='flex space-x-3 justify-center items-center align-middle gap-[85px] bg-opacity-100'>
         <Card color={"blue"} title={"UI&UX"} desc={"web design services and user experience research"}/>        
         <Card color={"yellow"} title={"WEBDEV"} desc={"creating Responsive and efficient web experiences"}/>
         <Card color={"red"} title={"Data Science"} desc={"working on this"}/>
       </div>
+      </div>
+      </div>
+
     </div>
   )
 }
